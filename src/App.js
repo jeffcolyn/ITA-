a2277a8a538b2bcea4781afd82f43bea5a54254f
jeffcolyn <jeferson.barbosa101@gmail.com>
2024-04-26 18:57:07 -0300

    itau

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Header from "./Components/Header/Header";
+import SectionHero from "./Components/SectionHero";
+import SectionService from "./Components/SectionService";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SectionHero />
+      <SectionService />
+    </>
+  );
+}
+
+export default App;
